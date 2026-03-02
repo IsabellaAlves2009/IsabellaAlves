db332c85964717c0bb4c5fe67fba48831af757e3
Isabella Alves <bella.s.alves2009@gmail.com>
2026-03-02 17:09:05 -0300

    fix: final adjustment to paths and casing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Hero } from './components/Hero/Hero'
 import About from './components/AboutMe/AboutMe';
 import { Skills3D } from "./components/Skills/Skills3D";
-import { ProjectSection } from './components/Projects/projectSection';
+import { ProjectSection } from './components/Projects/projectSection'; 
 import { Contact } from './components/Contact/Contact';
 
 function App() {
